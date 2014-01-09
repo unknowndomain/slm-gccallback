f2d8f6b627b7c5063bd36bef80e6a1b13a900d34
Matt Copperwaite <mattcopp+git@gmail.com>
2014-01-09 20:20:13 +0000

    added better error messages

--- app.js
+++ app.js
@@ -11,32 +11,37 @@ module.exports = {
         var gc = GoCardless(config.gocardless);
         
         app.post('/webhook/:key', function(req, res) {
-            if ((req.params.key == config.gocardless.secretKey) && (gocardless.webhookValid(req.body))) { // req.body is a guess
-                if (req.body.payload.resource_type == "bill") {
-                    if (req.body.payload.action == config.gocardless.paidWhen) { // can be either paid or withdrawn (i.e.: paid out)
-                        _.each(req.body.payload.bills, function (bill) {
-                            if ((bill.status == "paid") && (bill.source_type == "subscription")) {
-                                // should probably check the amount here
-                                User.findOne({where: {gc_subscription: bill.source_id}}, function (err, user) {
-                                    if (!err && user) {
-                                        user.last_payment = new Date();
-                                        user.save(function (err,user) {
-                                            if (err) {
-                                                console.log("Could not save user: " + user);
-                                            }
-                                        });
-                                    }
-                                });
-                            }
-                        });
-                        // should check here for billing issues
+            if (req.params.key == config.gocardless.secretKey) {
+                if (gocardless.webhookValid(req.body)) { // req.body is a guess
+                    if (req.body.payload.resource_type == "bill") {
+                        if (req.body.payload.action == config.gocardless.paidWhen) { // can be either paid or withdrawn (i.e.: paid out)
+                            _.each(req.body.payload.bills, function (bill) {
+                                if ((bill.status == "paid") && (bill.source_type == "subscription")) {
+                                    // should probably check the amount here
+                                    User.findOne({where: {gc_subscription: bill.source_id}}, function (err, user) {
+                                        if (!err && user) {
+                                            user.last_payment = new Date();
+                                            user.save(function (err,user) {
+                                                if (err) {
+                                                    console.log("Could not save user: " + user);
+                                                }
+                                            });
+                                        }
+                                    });
+                                }
+                            });
+                            // should check here for billing issues
+                        }
                     }
+                    // could check if it is an update about subscription cancellations
+                    return res.send(200);
                 }
-                // could check if it is an update about subscription cancellations
-                res.send(200);
+                else {
+                    return res.send(403, "invalid webhook signature.");
+                }
             }
             else {
-                return res.send(403);
+                return res.send(403, "incorrect secretkey. Got: " + req.params.key);
             }
         });
         
