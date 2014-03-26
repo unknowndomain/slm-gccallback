be55401f9bdc89c4855ab98a166a1a9fa8805c71
Matt Copperwaite <mattcopp+git@gmail.com>
2014-03-26 21:19:23 +0000

    now looks at bill for source type rather than payload

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ module.exports = {
                         if (req.body.payload.action == config.gocardless.paidWhen) { // can be either paid or withdrawn (i.e.: paid out)
                             _.each(req.body.payload.bills, function (bill) {
                                 if (bill.status == config.gocardless.paidWhen) {
-                                    if ((req.body.payload.resource_type == "subscription") && (bill.amount >= config.gocardless.minimum)) {
+                                    if ((bill.source_type == "subscription") && (parseFloat(bill.amount) >= config.gocardless.minimum)) {
                                         res.locals.User.findOne({where: {gc_subscription: bill.source_id}}, function (err, user) {
                                             if (!err && user) {
                                                 user.historic_events.create({
@@ -52,7 +52,7 @@ module.exports = {
                                             }
                                         });
                                     }
-                                    else if (req.body.payload.resource_type == "bill") {
+                                    else if (bill.source_type == "bill") {
                                         res.locals.User.findOne({where: {gc_donation: bill.source_id}}, function (err, user) {
                                             if (!err && user) {
                                                 user.historic_events.create({
